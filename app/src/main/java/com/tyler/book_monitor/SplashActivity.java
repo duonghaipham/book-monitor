1d4c3098005db502a509694adb2b4d9572499718
Tyler <phduongit2k1@gmail.com>
2022-05-24 21:43:54 +0700

    Splash screen and layout activity

---
+++ app/src/main/java/com/tyler/book_monitor/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.tyler.book_monitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2500;
+
+    private LottieAnimationView lavSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        lavSplash = findViewById(R.id.lav_splash);
+
+        lavSplash.animate().setDuration(SPLASH_TIME_OUT);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
